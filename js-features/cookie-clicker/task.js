4ed19e31961be72284f9d6902f780bc079882390
Laptop Gleb Bir <biryukov.gleb.98@mail.ru>
2020-03-28 21:31:13 +0300

    Не в том задание написано решение. Стер

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,12 +1,2 @@
-// Всего кликов <span id="clicker__counter">0</span>
-const clicker__counter = document.getElementById("clicker__counter");
-const reduceNumber = function(){
-    clicker__counter.textContent -= 1;
-    if ( clicker__counter.textContent < 0) {
-        clearInterval(id);
-        alert ("You win!");
-    }
-}
 
-const id = setInterval(reduceNumber, 1000);
  
