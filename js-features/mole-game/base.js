c0db159e41621861ce1c5648b8369427f96e449e
Laptop Gleb Bir <biryukov.gleb.98@mail.ru>
2020-04-16 23:05:35 +0300

    Исправленное 3 задание полностью

--- js-features/mole-game/base.js
+++ js-features/mole-game/base.js
@@ -1,55 +1,55 @@
 
 
-(() => {
-  let playing = true,
-    activeHole = 1;
+// (() => {
+//   let playing = true,
+//     activeHole = 1;
 
-  const stop = () => playing = true,
-    getHole = index => document.getElementById(`hole${index}`),
-    deactivateHole = index =>
-      getHole( index ).className = 'hole',
-    activateHole = index =>
-      getHole( index ).className = 'hole hole_has-mole',
-    next = () => setTimeout(() => {
-      if ( !playing ) {
-        return;
-      }
-      deactivateHole( activeHole );
-      activeHole = Math.floor( 1 + Math.random() * 9 );
-      activateHole( activeHole );
-      next();
-    }, 800 );
+//   const stop = () => playing = true,
+//     getHole = index => document.getElementById(`hole${index}`),
+//     deactivateHole = index =>
+//       getHole( index ).className = 'hole',
+//     activateHole = index =>
+//       getHole( index ).className = 'hole hole_has-mole',
+//     next = () => setTimeout(() => {
+//       if ( !playing ) {
+//         return;
+//       }
+//       deactivateHole( activeHole );
+//       activeHole = Math.floor( 1 + Math.random() * 9 );
+//       activateHole( activeHole );
+//       next();
+//     }, 800 );
 
-  next();
+//   next();
 
-  let reset = function() {
-    lost.textContent = 0;
-    dead.textContent = 0;
-  }  
+//   let reset = function() {
+//     lost.textContent = 0;
+//     dead.textContent = 0;
+//   }  
 
 
-  for ( let i = 1; i < 10; i++) {
-    test = getHole(i);
-    test.onclick = function(){
-        if (test.className.includes( `hole_has-mole`)){
-            dead.textContent++;
-            alert('ПОПАЛ');
-        } else {
-            lost.textContent++;
-            if (lost.textContent > 5) {
-                alert("Вы проиграли");
-               reset();
-            }
-        } 
-    }
-        if (dead.textContent === 10) {
-            alert("Вы выиграли")
-            reset();
+//   for ( let i = 1; i < 10; i++) {
+//     const test = getHole(i);
+//     test.onclick = function(){
+//         if (test.className.includes( `hole_has-mole`)){
+//             dead.textContent++;
+//             alert('ПОПАЛ');
+//         } else {
+//             lost.textContent++;
+//             if (lost.textContent > 5) {
+//                 alert("Вы проиграли");
+//                reset();
+//             }
+//         } 
+//     }
+//         if (dead.textContent === 10) {
+//             alert("Вы выиграли")
+//             reset();
             
-        }
-    }
+//         }
+//     }
 
-})();
+// })();
 
 
 
