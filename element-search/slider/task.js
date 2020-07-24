e12bb3e806239e647a9d83f6dfedb48f2018c6ab
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-07-24 17:58:24 +0300

    Исправил findIndex, сделал дополнительное задание

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,64 +4,55 @@ let slider_dots = [...document.querySelectorAll(".slider__dot")]
 
 let number = 0;
 
-let deactivateArrow = function(){
-    
-    console.log(slider_item.findIndex( (item) => {
-        item.classList.contains("slider__item_active");
-    }))
+let deactivateArrow = function(){    
+   let index = slider_item.findIndex( (item) => {
+        return item.classList.contains("slider__item_active");
+    })
+    slider_item[index].classList.remove("slider__item_active");
+    slider_item[number].classList.add("slider__item_active");
 };
     
-
-    // slider_item.forEach((element) => {
-    //     if (element.classList.contains("slider__item_active")){
-    //         element.classList.remove("slider__item_active")
-    //     }
-    // })
-    //     slider_item[number].classList.add("slider__item_active")  
-
-
-
 slider_arrow.forEach( function(element){
     element.onclick = function(){
         if (element.classList.contains("slider__arrow_next")){
-            if (number < slider_item.length - 1){
+            if (number < slider_item.length){
                 deactivateArrow();   
                     number++
-            }   else if (number >= slider_item.length - 1){
+            }else if (number = slider_item.length){
                 number = 0;
-            }
-        }
+            };
+        };
 
         if (element.classList.contains("slider__arrow_prev")){
-           if (number > 0){
-               deactivateArrow();
-               number--
-           }    else if ( number <= 0){
-                number = slider_item.length - 1;
-           }
-        }
-    }
-})
+            if (number >= 0){
+                deactivateArrow();
+                number--;
+            }else if (number < 0){
+                number = slider_item.length;
+                number--;
+            }; 
+            };
+        };
+    });
 
 // доп задание
 
 slider_dots.forEach( function (element){
-     
-        element.onclick = function(){ 
-            slider_dots.forEach (function (element){ 
-                
-            if (element.classList.contains("slider__dot_active")){
-                element.classList.remove("slider__dot_active");
-            }
-        });  
+     element.onclick = function(){ 
+            let dotIndex = slider_dots.findIndex( (dot) => {
+                return dot.classList.contains("slider__dot_active");
+            });
+                if (dotIndex >= 0){
+                    slider_dots[dotIndex].classList.remove("slider__dot_active");
+                    slider_item[dotIndex].classList.remove("slider__item_active");
+            };
+            
             element.classList.add("slider__dot_active");
+            let slideIndex = slider_dots.findIndex( (slide) => {
+                return slide.classList.contains("slider__dot_active");
+            })
+            slider_item[slideIndex].classList.add("slider__item_active");
         }
 })
 
-
-// slider_dots.map( function (element){
-//     slider_item.map (function (items){
-//          return element = items;
-//     })
-// })
 
