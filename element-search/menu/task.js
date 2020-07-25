0957b5ba33d6972fe2d6fbaca78e587afe37d204
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-07-25 16:19:17 +0300

    Исправлено ошибка classlist undefined при 1 и 4 вкладке

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,19 +1,33 @@
 let menu_link = [...document.querySelectorAll(".menu__link")];
 let menu_sub = [...document.querySelectorAll(".menu_sub")]
 
+
+let menuSubDeactivate = function(){
+        let menuSubIndex = menu_sub.findIndex( function (element){
+                return element.classList.contains("menu_active");
+        })
+        if (menuSubIndex >= 0){
+        menu_sub[menuSubIndex].classList.remove("menu_active");
+};
+}
+
+
 menu_link.forEach(function (element) {
         element.onclick = function(){
+                let elementClass = element.closest("li").querySelector("ul");
+                if (elementClass === null){
+                        menuSubDeactivate();
+                        console.log(`wotktrk`);
+                        return false;
+                } else if (elementClass){
+                        console.log(elementClass);
+                        if (elementClass.classList.contains("menu_active")){
+                                elementClass.classList.remove("menu_active");
+                        }
 
-        if (element.closest("li").querySelector("ul").classList.contains("menu_active")){
-            element.closest("li").querySelector("ul").classList.remove("menu_active");
-        } 
-            else if (element.closest("li").querySelector("ul")){
-                menu_sub.forEach(function (element) {
-                element.classList.remove("menu_active");
-        })
-                this.closest("li").querySelector("ul").classList.add("menu_active");
-        }
-                return false;
-        }
-})
-
+                        menuSubDeactivate();
+                        elementClass.classList.add("menu_active");
+                        return false;
+                };
+        };
+});
