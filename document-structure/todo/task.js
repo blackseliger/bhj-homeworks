9616d3c11537e7ef2e180b156f911d956e7f1c39
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-09 14:05:31 +0300

    Происходит обновление страницы после ввода

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,9 +1,16 @@
 let tasks_input = document.querySelector(`.tasks__input`);
 let taslsList = document.querySelector(`.tasks__list`);
 
-tasks_input.addEventListener(`change`, (e) => {
+tasks_input.addEventListener(`change`, (e) => { 
     e.preventDefault();
-    console.log(`check work`)
-    
-})
+    element = e.target;
+
+    console.log(`its work`);
+    taslsList.insertAdjacentHTML(`beforeEnd`, `<div class="task">
+    <div class="task__title">
+      ${e.target.innerText}
+    </div>
+    <a href="#" class="task__remove">&times;</a>
+  </div> `)
+});
 
