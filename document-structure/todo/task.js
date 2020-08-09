6df18215c0c72e92772a14c1d3d0165bd7b1e39b
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-09 13:36:58 +0300

    Страница обновляется после инпута

---
+++ document-structure/todo/task.js
@@ -0,0 +1,9 @@
+let tasks_input = document.querySelector(`.tasks__input`);
+let taslsList = document.querySelector(`.tasks__list`);
+
+tasks_input.addEventListener(`change`, (e) => {
+    e.preventDefault();
+    console.log(`check work`)
+    
+})
+
