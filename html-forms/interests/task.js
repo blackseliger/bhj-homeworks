9540abe6389dfa2e76724f8f8542a8a5aea3755e
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-09 18:06:30 +0300

    Сделал основное задание

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -5,9 +5,7 @@ let interestCheck = [...document.querySelectorAll(`.interest__check`)];
 
 interestCheck.forEach( interest => {
     interest.addEventListener(`change`, e => {
-        
         let listCheck = e.currentTarget.closest(`.interest`).querySelector(`.interests_active`);
-        console.log(!(listCheck === null))
         if (!(listCheck === null)){
             let listInterests = [...listCheck.querySelectorAll(`.interest__check`)];
             let bool = e.currentTarget.checked === false ? false : true; 
@@ -15,9 +13,15 @@ interestCheck.forEach( interest => {
                 e.checked = bool;
         })  
         } else {
-            let mainCheck = e.currentTarget.closest(`.interests_active`).closest(`.interest`).querySelector(`.interest__check`);
-            return e.currentTarget.checked === true ? mainCheck.checked = true : mainCheck.checked = false;
-        }
+            let checklist2lvl = [...e.currentTarget.closest(`.interests_active`).querySelectorAll(`.interest__check`)];
+            let check2LVL = checklist2lvl.every( interest => interest.checked === true);
+            let listInterests1lvl = e.currentTarget.closest(`.interests_active`).closest(`.interest`).querySelector(`.interest__check`);
+            check2LVL === true ? listInterests1lvl.checked = true : listInterests1lvl.checked = false;
+        };
+        // else {
+        //     let mainCheck = e.currentTarget.closest(`.interests_active`).closest(`.interest`).querySelector(`.interest__check`);
+        //     return e.currentTarget.checked === true ? mainCheck.checked = true : mainCheck.checked = false;
+        // }
     })
 })
 
