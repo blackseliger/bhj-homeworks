72678c5e0085293d96f7f5876a690ba26db846db
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-10 18:59:13 +0300

    Не получается назначить событие на крестик

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,16 +1,55 @@
 let tasks_input = document.querySelector(`.tasks__input`);
-let taslsList = document.querySelector(`.tasks__list`);
+let tasksList = document.querySelector(`.tasks__list`);
+let tasksControl = document.querySelector(`.tasks__control`);
+// let task_remove = [...document.querySelectorAll(`task__remove`)];
 
-tasks_input.addEventListener(`change`, (e) => { 
+let tasks_remove = [];
+let counter = 0;
+
+tasksControl.addEventListener(`submit`, e => {
+  e.preventDefault(); // предотвратить перезагрузку страницы
+  element = e.target;
+  
+  if (!(tasks_input.value === ``)){
+  tasksList.insertAdjacentHTML(`beforeEnd`, `<div class="task">
+  <div class="task__title">
+    ${tasks_input.value}
+  </div>
+  <a href="#" class="task__remove">&times;</a>
+</div> `)
+
+    let task_remove = [...document.querySelectorAll(`.task__remove`)];
+    task_remove = task_remove[counter]
+    counter++;
+    return tasks_remove.push(task_remove);
+  };
+
+    element.reset();
+})
+
+for (let i = 0; i < tasks_remove.length; i++){
+  tasks_remove[i].addEventListener(`click`, e => {
+      e.preventDefault();
+      console.log(`its work`);
+  })
+}
+
+tasks_remove.forEach( element => {
+  element.addEventListener(`click`, e => {
     e.preventDefault();
-    element = e.target;
+    console.log(`its wotk`);
+  })
+})
 
-    console.log(`its work`);
-    taslsList.insertAdjacentHTML(`beforeEnd`, `<div class="task">
-    <div class="task__title">
-      ${e.target.innerText}
-    </div>
-    <a href="#" class="task__remove">&times;</a>
-  </div> `)
-});
+
+// task_remove.forEach( element => {
+//   element.addEventListener(`click`, e => {
+//     e.preventDefault();
+//     console.log(`its work`)
+//     element = e.currentTarget;
+//     let removeEl = element.closest(`.task`);
+//     console.log(removeEl);
+//   })
+// })
+
 
