bc6109c613b6716a760e12929a3aad87f8c78feb
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-18 11:42:07 +0300

    Добавил проверку в локалсторадж

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -3,11 +3,29 @@ let textArea = document.getElementById(`editor`);
 
 
 document.addEventListener(`DOMContentLoaded`, e => {
-    textArea.value = localStorage.text;
+    if (localStorage.text){
+        textArea.value = localStorage.text;
+    };
 })
 
+// window.addEventListener(`beforeunload`, e => {
+//         textArea.value = localStorage.text;
+//         e.returnValue = textArea.value;
+//         return textArea.value = localStorage.text;
+// })
 
-textArea.addEventListener(`change`, e => {
+// window.onbeforeunload = function(e){
+//     textArea.value = localStorage.text;
+//     e.returnValue = textArea.value;
+//     return textArea.value;
+// } 
+
+textArea.addEventListener(`input`, e => {
     element = e.currentTarget;
     localStorage.setItem(`text`, `${element.value}`);
 })
+
+
+
+// url error https://recordit.co/iEy6d3n3fj
+// Вылетает окошко с вопросом, и только. С 11 по 16, с 17 по 21. Одинаковые свойства
