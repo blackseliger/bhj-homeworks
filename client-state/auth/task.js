fbc75e6d0ec632340281b8b550dcb81ffc913114
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-18 12:29:18 +0300

    Сделанное основное

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,11 +1,14 @@
 let formId = document.getElementById(`signin__form`);
 let user_id = document.getElementById(`user_id`);
+let signin = document.querySelector(`.signin`);
 
 
 document.addEventListener(`DOMContentLoaded`, e => {
     if (localStorage.user){
         user_id.innerText = localStorage.user;
         user_id.closest(`.welcome`).classList.add(`welcome_active`);
+    } else {
+        signin.classList.add(`signin_active`);
     }
 })
 
@@ -27,6 +30,7 @@ formId.addEventListener(`submit`, e => {
                 user_id.innerText = answer[1];
                 user_id.closest(`.welcome`).classList.add(`welcome_active`);
                 localStorage.setItem(`user`, `${answer[1]}`);
+                signin.classList.remove(`signin_active`);
             }
         }
     })
