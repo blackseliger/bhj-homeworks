3f80e21d1ed35a9f74b2856ff633fe8eaedc2169
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-19 17:41:26 +0300

    Completed task

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,11 +1,10 @@
-let pollAnswer = [...document.querySelectorAll(`.poll__answer`)];
+
 let pollTitle = document.querySelector(`.poll__title`);
 let pollAnswers = document.querySelector(`.poll__answers`);
 
 
-pollAnswer.forEach(element => {
-    element.addEventListener(`click`, e => {
-        let element = e.currentTarget;
+pollAnswers.addEventListener(`click`, e => {
+        let element = e.target;
         let xhr = new XMLHttpRequest();
         xhr.open(`GET`, `https://netology-slow-rest.herokuapp.com/poll.php`, true);
         xhr.addEventListener(`readystatechange`, () => {
@@ -24,38 +23,99 @@ pollAnswer.forEach(element => {
                 }
                 let titleValue = arrDataValues[index(`title`)];
                 let answersValue = arrDataValues[index(`answers`)];
-                console.log(answersValue);
                 pollTitle.innerText = titleValue;
-                let checkActualArrPollAnswer = document.querySelectorAll(`.poll__answer`)
-
-                if (answersValue.length > checkActualArrPollAnswer.length){
-                    console.log(`${answersValue.length} > ${checkActualArrPollAnswer.length}`);
-                    let difference = answersValue.length -  checkActualArrPollAnswer.length;
-                    console.log(difference);
-                    let clonePollAnswer = document.createElement(`button`);
-                    clonePollAnswer.classList.add(`poll__answer`);
-                    
-                    for (let i = 0; i < difference; i++){
-                        // pollAnswers.appendChild(clonePollAnswer);
-                        pollAnswers.insertAdjacentHTML(`beforeend`, `<button class="poll__answer">
-                        Кто тут?
-                      </button>`)
-                        console.log(checkActualArrPollAnswer.length); //добавил элемент, но длина массива не изменилась
-                    }
-                }   else if (answersValue.length < checkActualArrPollAnswer.length){
-                    console.log(`${answersValue.length} < ${checkActualArrPollAnswer.length}`);
-                    for (let i = answersValue.length; i < checkActualArrPollAnswer.length; i++){
-                        checkActualArrPollAnswer.item(i).remove();
-                        console.log(checkActualArrPollAnswer.length); 
-                    } // тоже не изменяется длина массива. Самурай в поту, мысли роят в голове, не может понять сложную механику Джаваскрипта
-                };   
-                    for (let i = 0; i < checkActualArrPollAnswer.length; i++){
-                        checkActualArrPollAnswer[i].innerText = answersValue[i];
-                    };
-            } else {
-                console.log(xhr.readyState);
-            }
+                let pollAnswer = [...document.querySelectorAll(`.poll__answer`)];
+                pollAnswer.forEach(element => {
+                   return element.remove();
+                });
+                
+                for (let i = 0; i < answersValue.length; i++ ){
+                    let pollAnswerNew = document.createElement(`button`);
+                    pollAnswerNew.classList.add(`poll__answer`);
+                    pollAnswerNew.innerText = answersValue[i]
+                    pollAnswers.appendChild(pollAnswerNew);
+                }
+            };
         })
         xhr.send();
     })
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // if (answersValue.length > document.querySelectorAll('.poll__answer').length){
+                //     console.log(`${answersValue.length} > ${document.querySelectorAll('.poll__answer').length}`);
+                //     let difference = answersValue.length -  document.querySelectorAll('.poll__answer').length;
+                //     let clonePollAnswer = document.createElement(`button`);
+                //     clonePollAnswer.classList.add(`poll__answer`);
+                    
+                //     for (let i = 0; i < difference; i++){
+                //         pollAnswers.insertAdjacentHTML(`beforeend`, `<button class="poll__answer">
+                //         Кто тут?
+                //       </button>`)
+                //     }
+                // }   else if (answersValue.length < document.querySelectorAll('.poll__answer').length){
+                //     console.log(`${answersValue.length} < ${document.querySelectorAll('.poll__answer').length}`);
+                //     for (let i = answersValue.length; i < document.querySelectorAll('.poll__answer').length; i++){
+                //         document.querySelectorAll('.poll__answer').item(i).remove();
+                //     }
+                // };   
+                //     for (let i = 0; i < document.querySelectorAll('.poll__answer').length; i++){
+                //         document.querySelectorAll('.poll__answer')[i].innerText = answersValue[i];
+                //     };
+
+
+
+
+
+                // pollAnswer.forEach(element => {
+                //     element.addEventListener(`click`, e => {
+                //         let xhr = new XMLHttpRequest();
+                //         xhr.open(`GET`, `https://netology-slow-rest.herokuapp.com/poll.php`, true);
+                //         xhr.addEventListener(`readystatechange`, () => {
+                //             if (xhr.readyState === 4 && xhr.status === 200){
+                //                 let data = JSON.parse(xhr.responseText);
+                //                 // alert(`Спасибо, ваш голос засчитан`)
+                                
+                //                 let arrDataKeys = Object.keys(data.data);
+                //                 let arrDataValues = Object.values(data.data);
+                                
+                //                 let index = function(indexName){
+                //                     let findIndex = arrDataKeys.findIndex( element => {
+                //                         return element ===  indexName 
+                //                     });
+                //                         return findIndex;
+                //                 }
+                //                 let titleValue = arrDataValues[index(`title`)];
+                //                 let answersValue = arrDataValues[index(`answers`)];
+                //                 pollTitle.innerText = titleValue;
+                //                 [...document.querySelectorAll(`.poll__answer`)].forEach(element => {
+                //                    return element.remove();
+                //                 });
+                                
+                //                 for (let i = 0; i < answersValue.length; i++ ){
+                //                     let pollAnswerNew = document.createElement(`button`);
+                //                     pollAnswerNew.classList.add(`poll__answer`);
+                //                     pollAnswerNew.innerText = answersValue[i]
+                //                     pollAnswers.appendChild(pollAnswerNew);
+                //                 }
+                //                 console.log(pollAnswer)
+                //                 console.log([...document.querySelectorAll(`.poll__answer`)]);
+                               
+                //             };
+                //         })
+                //         xhr.send();
+                //     })
+                // })
