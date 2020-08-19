45fd2de547a3f7ddba8ef6cf69b93993bf63f09a
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-19 22:38:32 +0300

    Completed full task

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,22 +1,10 @@
 let modalCloseTimes = document.querySelector(`.modal__close_times`);
 let modal = document.querySelector(`.modal`);
 
-const getCookie = (name) => {
-    const value = "; " + document.cookie;  // Что делает эта строка? Функция вся взята из конспекта лекции
-    let parts = value.split("; " + name + "=");
-    if (parts.length === 2) {
-    return parts
-    .pop()
-    .split(";")
-    .shift();
-    }
-}
-
 document.addEventListener(`DOMContentLoaded`, e => {
-    let checkClass = getCookie(`modalView`);
-    if (checkClass.includes(`modal_active`) === false){
+    if (document.cookie.includes(`modalView`)){
         modal.classList.remove(`modal_active`);
-    }
+    };
 })
 
 
