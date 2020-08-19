4a514f0f2cdad016d8e177258b489063156f2af7
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-19 18:56:51 +0300

    Проблема с dataset

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,7 +5,7 @@ let tooltip = document.querySelector(`.tooltip`);
 has_tooltip.forEach(el => {
   el.addEventListener(`click`, e => {
     e.preventDefault();
-    element = e.currentTarget;
+    element = e.target;
     let left = element.getBoundingClientRect().left;
     let top = element.getBoundingClientRect().bottom;
     let title = element.getAttribute(`title`);
@@ -17,8 +17,16 @@ has_tooltip.forEach(el => {
     } 
     
     tooltip.innerText = `${title}`;
-    tooltip.style.top = `${top}px`;
-    tooltip.style.left = `${left}px`;
+    // tooltip.style.top = `${top}px`;
+    // tooltip.style.left = `${left}px`;
+    console.log(element.dataset.position); // пустая строчка
+    console.log(element.dataset.position.top)
+    tooltip.dataset.top = `20000px`  // ничего не происходи
+    tooltip.dataset.left = `7747px` .// тоже ничего
+
+    element.dataset.position = `${left}px`
+      tooltip.dataset.position.top = `200px`
+      console.log(tooltip.dataset.positionTop);
     // tooltip.dataset.position = `${top}px`;
     // tooltip.dataset.position = `right`; 
     // console.log(tooltip.dataset.position.top); undefined 
@@ -26,3 +34,4 @@ has_tooltip.forEach(el => {
     //  Не до конца понимаю, как работать с атрибутом data-position...
   })
 })
+
